5aaee704342f53f01b914e1f3975d76b2dce0881
Eugene Bendik <ebendik@gmail.com>
2019-09-02 16:44:56 -0600

    adding algo

--- Algo/Algo/main.cpp
+++ Algo/Algo/main.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
+#include "prime_factorization.h"
+#include "gcd.h"
 
+
+void test_prime_factorization()
+{
+    auto prim_fac = prime_factorization(1560000000);
+    
+    for(const auto& num: prim_fac)
+    {
+        std::cout << num << ", ";
+    }
+    std::cout << std::endl;
+}
+
+void test_greatest_commond_divisor()
+{
+    std::cout << greatest_common_divisor(20, 3) << std::endl;
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+//    test_prime_factorization();
+    test_greatest_commond_divisor();
+    
+    
     return 0;
 }
